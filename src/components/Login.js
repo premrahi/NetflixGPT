3de2716d61a8ff231d434fd884e9ebd7b0de0772
premrahi <premrahi234@gmail.com>
2026-03-07 14:19:41 +0530

    added responsiveness in login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,10 +9,11 @@ import {
 import { auth } from "../utils/firebase";
 import { addUser } from "../utils/userSlice";
 import { useDispatch } from "react-redux";
-import { Login_BG, USER_IMG } from "../utils/constant"
+import { Login_BG, USER_IMG } from "../utils/constant";
 
 const Login = () => {
-  const dispatch = useDispatch() ;
+  const dispatch = useDispatch();
+
   const [signInState, setSignInState] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
@@ -34,100 +35,105 @@ const Login = () => {
     setErrorMessage(msg);
     if (msg) return;
 
+    // SIGN UP
     if (!signInState) {
       createUserWithEmailAndPassword(auth, emailValue, passwordValue)
         .then((userCredential) => {
-          // console.log(userCredential.user);
+          const user = userCredential.user;
 
-          const user = userCredential.user ;
           updateProfile(user, {
             displayName: nameValue,
-            photoURL: USER_IMG ,
+            photoURL: USER_IMG,
           })
             .then(() => {
-              const {uid ,email ,displayName , photoURL} = auth.currentUser ; 
-              // auth.current user gets the value form updated user. just "user" has no updated values yet.
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+
               dispatch(
-                addUser({uid : uid , email : email , displayName : displayName , photoURL : photoURL}) 
-              )
-              // Profile updated!
-              // ...
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
             })
             .catch((error) => {
-              setErrorMessage(error.message) ;
+              setErrorMessage(error.message);
             });
-
         })
         .catch((error) => {
-          setErrorMessage(error.code + "-" + error.message);
+          setErrorMessage(error.code + " - " + error.message);
         });
-    } else {
+    }
+
+    // SIGN IN
+    else {
       signInWithEmailAndPassword(auth, emailValue, passwordValue)
-        .then((userCredential) => {
-          // console.log(userCredential.user);
-        })
+        .then(() => {})
         .catch((error) => {
-          setErrorMessage(error.code + "-" + error.message);
+          setErrorMessage(error.code + " - " + error.message);
         });
     }
   };
 
   return (
-    <>
-      <div>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${Login_BG})` }}
+    >
+      {/* Dark overlay */}
+      <div className="min-h-screen bg-black/60">
         <Header />
-        <div className="absolute">
-          <img
-            src={Login_BG}  alt="bg"
-          ></img>
-        </div>
 
         <form
           onSubmit={(e) => e.preventDefault()}
-          className="absolute w-3/12 my-28 bg-opacity-80 bg-black mx-auto left-0 right-0 text-white "
+          className="absolute w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 my-28 p-10 bg-black bg-opacity-80 text-white left-0 right-0 mx-auto rounded-md"
         >
-          <div className="m-2 p-8 flex flex-col">
-            <h1 className="text-white  p-4 text-3xl font-semibold">
-              {signInState ? "Sign In" : "Sign Up"}
-            </h1>
-            {!signInState && (
-              <input
-                ref={name}
-                type="text"
-                placeholder="Name"
-                className="m-2 p-2  bg-gray-700 rounded-md w-3/3"
-              ></input>
-            )}
-            <input
-              ref={email}
-              type="text"
-              placeholder="Enter Email"
-              className="m-2 p-2  bg-gray-700 rounded-md w-3/3"
-            ></input>
+          <h1 className="text-3xl font-semibold mb-6">
+            {signInState ? "Sign In" : "Sign Up"}
+          </h1>
 
+          {!signInState && (
             <input
-              type="password"
-              ref={password}
-              placeholder="password"
-              className="m-2 p-2  bg-gray-700 rounded-md w-3/3"
-            ></input>
-            <p className="text-red-700 font-semibold p-2">{errorMessage}</p>
-            <button
-              type="submit"
-              className="m-2 p-2 my-8 rounded-md border bg-red-600 text-white border-black w-3/3"
-              onClick={() => handleBtnClick()}
-            >
-              {signInState ? "Sign In" : "Sign Up"}
-            </button>
-            <p className="p-4 cursor-pointer" onClick={toggleSignINForm}>
-              {signInState
-                ? "New to Netflix? Sign Up now!"
-                : "Already registered?, Sign In now!"}
-            </p>
-          </div>
+              ref={name}
+              type="text"
+              placeholder="Name"
+              className="p-3 my-3 w-full bg-gray-700 rounded-md"
+            />
+          )}
+
+          <input
+            ref={email}
+            type="text"
+            placeholder="Enter Email"
+            className="p-3 my-3 w-full bg-gray-700 rounded-md"
+          />
+
+          <input
+            ref={password}
+            type="password"
+            placeholder="Password"
+            className="p-3 my-3 w-full bg-gray-700 rounded-md"
+          />
+
+          <p className="text-red-500 text-sm py-2">{errorMessage}</p>
+
+          <button
+            type="submit"
+            className="p-3 my-6 w-full bg-red-600 rounded-md font-semibold hover:bg-red-700 transition"
+            onClick={handleBtnClick}
+          >
+            {signInState ? "Sign In" : "Sign Up"}
+          </button>
+
+          <p className="cursor-pointer text-gray-300" onClick={toggleSignINForm}>
+            {signInState
+              ? "New to Netflix? Sign Up now!"
+              : "Already registered? Sign In now!"}
+          </p>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
